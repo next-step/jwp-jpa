df7b79a5d3d1c955ba33e36723cd00581991b6a3
cionman <topspin1278@naver.com>
2020-12-14 23:24:29 +0900

    feat : 공통 칼럼 BaseEntity 추가

---
+++ src/main/java/jpa/domain/common/BaseEntity.java
@@ -0,0 +1,32 @@
+package jpa.domain.common;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_date", updatable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	@Column(name = "modified_date")
+	private LocalDateTime modifiedDate;
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+}
