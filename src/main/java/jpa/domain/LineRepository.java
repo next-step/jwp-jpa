9374b4df4c4ebdd408c5ab14d174dde513ef73c5
cionman <topspin1278@naver.com>
2020-12-15 08:55:03 +0900

    feat : LineRepository, StationRepository findByName 메소드 추가

--- src/main/java/jpa/domain/LineRepository.java
+++ src/main/java/jpa/domain/LineRepository.java
@@ -3,4 +3,5 @@ package jpa.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
+	Line findByName(String name);
 }
--- src/main/java/jpa/domain/StationRepository.java
+++ src/main/java/jpa/domain/StationRepository.java
@@ -3,4 +3,5 @@ package jpa.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+	Station findByName(String name);
 }
