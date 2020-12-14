7fcfe6811d9b9c02b9f55f0951e195bdca7fef71
wotjd243 <wotjd243@naver.com>
2020-12-14 10:00:04 +0900

    feat(jpa): set up the project

---
+++ src/test/java/jpa/ApplicationTest.java
@@ -0,0 +1,11 @@
+package jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
