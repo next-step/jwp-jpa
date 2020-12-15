a883a9ad5540eddadcadb51b230c56f1fe9140ac
Ssabae <llsb156@gmail.com>
2020-12-15 18:41:45 +0900

    feat: Domain Entity 생성

---
+++ src/main/java/jpa/domain/Member.java
@@ -0,0 +1,52 @@
+package jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import jpa.infrastructure.jpa.BaseEntity;
+
+/**
+ * @author : leesangbae
+ * @project : jpa
+ * @since : 2020-12-15
+ */
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private Integer age;
+
+    public Member() {
+    }
+
+    public Member(String email, String password, Integer age) {
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
