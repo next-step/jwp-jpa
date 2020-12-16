d544313822e22a8185d2078465b5b7c5ac7f863e
Pilhwan Kim <pilhwankim03@gmail.com>
2020-12-16 22:28:28 +0900

    feat: entity 작성

---
+++ src/main/java/jpa/entity/Favorite.java
@@ -0,0 +1,15 @@
+package jpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Favorite extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+}
