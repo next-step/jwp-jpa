96fdd07127adb37029bdbc67001d599920bdd2dd
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-19 21:07:37 +0900

    test(LineRepositoryTest & BaseTest): extract super test class

--- src/test/java/jpa/com/jaenyeong/domain/line/LineRepositoryTest.java
+++ src/test/java/jpa/com/jaenyeong/domain/line/LineRepositoryTest.java
@@ -1,14 +1,10 @@
 package jpa.com.jaenyeong.domain.line;
 
-import jpa.com.jaenyeong.domain.line.Line;
-import jpa.com.jaenyeong.domain.line.LineRepository;
+import jpa.com.jaenyeong.domain.BaseTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
@@ -16,13 +12,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
-@DataJpaTest
-// 테스트 시 H2 DB를 사용하기 위하여 애노테이션 추가
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// CreateDate, LastModifiedDate 초기화를 위해 추가
-@EnableJpaAuditing
 @DisplayName("Line Repository 테스트")
-class LineRepositoryTest {
+class LineRepositoryTest extends BaseTest {
     @Autowired
     private LineRepository lines;
 
