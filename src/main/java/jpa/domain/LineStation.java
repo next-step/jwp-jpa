e3b277369577cd1507ace844b6886297b1c16dbf
poppohoo <ccf05017@gmail.com>
2020-12-19 14:44:00 +0900

    feat(LineStation): bridge object for many to many mapping
    - Line, Station 간 다대다 관계 매핑을 진행하기 위한 중간 객체 및 리포지토리 구현

---
+++ src/main/java/jpa/domain/LineStation.java
@@ -0,0 +1,47 @@
+package jpa.domain;
+
+import jpa.utils.BaseEntity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class LineStation extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id")
+    private Line line;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "station_id")
+    private Station station;
+
+    protectedgit  LineStation() {
+    }
+
+    LineStation(final Long id, final Line line, final Station station) {
+        this.id = id;
+        this.line = line;
+        this.station = station;
+    }
+
+    LineStation(final Line line, final Station station) {
+        this(null, line, station);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final LineStation that = (LineStation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
