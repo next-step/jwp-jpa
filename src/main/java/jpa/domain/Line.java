edd75ba73e5752285c77522bde477375501fe19c
bss <baekss0409@gmail.com>
2020-12-19 17:47:55 +0900

    feat: entity 정의

---
+++ src/main/java/jpa/domain/Line.java
@@ -0,0 +1,38 @@
+package jpa.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Line extends BaseDateTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String color;
+
+    @Column(unique = true)
+    private String name;
+
+    protected Line() {}
+
+    public Line(String color, String name) {
+        this.color = color;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
