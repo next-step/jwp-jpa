f0f44d9599aed23b109af3c0623a172ca16cf88d
jaenyeong <jaenyeong.dev@gmail.com>
2020-12-20 21:35:43 +0900

    refactor(BaseTest): remove @AutoConfigureTestDatabase annotation

--- src/test/java/jpa/com/jaenyeong/domain/BaseTest.java
+++ src/test/java/jpa/com/jaenyeong/domain/BaseTest.java
@@ -1,12 +1,9 @@
 package jpa.com.jaenyeong.domain;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @DataJpaTest
-// 테스트 시 H2 DB를 사용하기 위하여 애노테이션 추가
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 // CreateDate, LastModifiedDate 초기화를 위해 추가
 @EnableJpaAuditing
 public class BaseTest {
