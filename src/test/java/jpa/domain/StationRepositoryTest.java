4debb0bfc69f72aee50a2bf73b8fce943a315f38
hhlyn0328 <hhlyn0328@daum.net>
2020-12-20 16:37:14 +0900

    feat: 노선과 지하철역 간의 ManyToMany 연관관계 추가

--- src/test/java/jpa/domain/StationRepositoryTest.java
+++ src/test/java/jpa/domain/StationRepositoryTest.java
@@ -3,6 +3,9 @@ package jpa.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,9 +13,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 @DataJpaTest
 class StationRepositoryTest {
-	
 	@Autowired
 	private StationRepository stationRepository;
+	@Autowired
+	private LineRepository lineRepository;
 
 	@Test
 	void saveTest() {
@@ -81,5 +85,55 @@ class StationRepositoryTest {
 		// save 시점에 exception이 될 줄 알았지만 쓰기 지연으로 인해 에러가 나지 않음 -> findByName을 할 때 flush 되며 발생
 		// assertThatThrownBy(() -> stationRepository.save(newStation)).isInstanceOf(
 		// 	DataIntegrityViolationException.class);
+	}
+
+	@Test
+	@DisplayName("지하철역 조회 시 속한 노선을을 볼 수 있다.")
+	public void stationsInLineTest() {
+		String expectedName = "문래역";
+		Station station = stationRepository.save(new Station(expectedName));
+
+		Line line1 = lineRepository.save(new Line("1호선", "BLUE"));
+		Line line2 = lineRepository.save(new Line("2호선", "GREEN"));
+		Line line3 = lineRepository.save(new Line("3호선", "ORANGE"));
+		Line line4 = lineRepository.save(new Line("4호선", "SKYBLUE"));
+
+		station.addLines(Arrays.asList(line1, line2, line3));
+		Station actual = stationRepository.findByName(expectedName);
+
+		assertThat(actual.getLines().size()).isEqualTo(3);
+		assertThat(actual.getLines().contains(line1)).isTrue();
+		assertThat(actual.getLines().contains(line2)).isTrue();
+		assertThat(actual.getLines().contains(line3)).isTrue();
+		assertThat(actual.getLines().contains(line4)).isFalse();
+	}
+
+	@Test
+	@DisplayName("지하철역에 노선 추가 시, 노선에도 지하철역이 추가되어야한다.")
+	public void addStationsInLineTest() {
+		String expectedStationName = "당산역";
+		String expectedLine1Name = "2호선";
+		String expectedLine2Name = "9호선";
+		Station station = stationRepository.save(new Station(expectedStationName));
+		Line line1 = lineRepository.save(new Line(expectedLine1Name, "GREEN"));
+		Line line2 = lineRepository.save(new Line(expectedLine2Name, "GOLD"));
+
+		assertThat(station.getLines().size()).isZero();
+		assertThat(line1.getStations().size()).isZero();
+		assertThat(line2.getStations().size()).isZero();
+
+		station.addLines(Arrays.asList(line1, line2));
+
+		Station actualStation = stationRepository.findByName(expectedStationName);
+		Line actualLine1 = lineRepository.findByName(expectedLine1Name);
+		Line actualLine2 = lineRepository.findByName(expectedLine2Name);
+
+		assertThat(actualStation.getLines().size()).isEqualTo(2);
+		assertThat(actualStation.getLines().contains(actualLine1)).isTrue();
+		assertThat(actualStation.getLines().contains(actualLine2)).isTrue();
+		assertThat(actualLine1.getStations().size()).isEqualTo(1);
+		assertThat(actualLine1.getStations().contains(actualStation)).isTrue();
+		assertThat(actualLine2.getStations().size()).isEqualTo(1);
+		assertThat(actualLine2.getStations().contains(actualStation)).isTrue();
 	}
 }
