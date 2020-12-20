2c100ff54fb47b9fdd3dc5f679ffb8879542b57a
wehwan <we_hwan@naver.com>
2020-12-21 00:29:01 +0900

    test: line 테스트 수정

--- src/test/java/jpa/domain/LineRepositoryTest.java
+++ src/test/java/jpa/domain/LineRepositoryTest.java
@@ -14,27 +14,27 @@ public class LineRepositoryTest {
     LineRepository lineRepository;
 
     @Test
-    @DisplayName("엔티티 저장 테스트")
+    @DisplayName("line 저장 테스트")
     public void save() {
         // given
         Line line = new Line("4호선");
 
         // when
-        Line saveLine = lineRepository.save(line);
+        Line saveLine = this.lineRepository.save(line);
 
         // then
         assertThat(saveLine).isEqualTo(line);
     }
 
     @Test
-    @DisplayName("엔티티 조회 테스트")
+    @DisplayName("이름으로 line 조회 테스트")
     public void findByName() {
         // given
         String name = "4호선";
         lineRepository.save(new Line(name));
 
         // when
-        Line lineFindByName = lineRepository.findByName(name);
+        Line lineFindByName = this.lineRepository.findByName(name);
 
         // then
         assertThat(lineFindByName.getName()).isEqualTo(name);
