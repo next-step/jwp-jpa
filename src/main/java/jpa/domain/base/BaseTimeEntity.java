f86fae47943b4195334bcb07a1b71184a9203f48
slamdunk7575 <slamdunk7575@gmail.com>
2020-12-20 16:01:25 +0900

    (feat) 공통 부분 엔티티 생성

---
+++ src/main/java/jpa/domain/base/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package jpa.domain.base;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+}
