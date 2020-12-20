789fa890a3fe5ecf1687eaf040f65fa12c3b8746
wehwan <we_hwan@naver.com>
2020-12-21 00:31:00 +0900

    feat: 역(Station) 엔티티 추가

---
+++ src/main/java/jpa/domain/Station.java
@@ -0,0 +1,56 @@
+package jpa.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "station")
+public class Station {
+
+    @Id @GeneratedValue
+    @Column(nullable = false)
+    private Long id;
+
+    private Date created_date;
+
+    private Date modified_date;
+
+    @Column(unique = true)
+    private String name;
+
+    public Station(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public Date getModified_date() {
+        return modified_date;
+    }
+
+    public void setModified_date(Date modified_date) {
+        this.modified_date = modified_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/jpa/domain/StationRepositoryTest.java
@@ -0,0 +1,42 @@
+package jpa.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class StationRepositoryTest {
+
+    @Autowired
+    StationRepository stationRepository;
+
+    @Test
+    @DisplayName("station 저장 테스트")
+    public void save() {
+        // given
+        Station station = new Station("산본역");
+
+        // when
+        Station saveStation = this.stationRepository.save(station);
+
+        // then
+        assertThat(saveStation).isEqualTo(station);
+    }
+
+    @Test
+    @DisplayName("이름으로 station 조회 테스트")
+    public void findByName() {
+        // given
+        String name = "산본역";
+        stationRepository.save(new Station(name));
+
+        // when
+        Station stationFindByName = this.stationRepository.findByName(name);
+
+        // then
+        assertThat(stationFindByName.getName()).isEqualTo(name);
+    }
+}
