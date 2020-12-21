58bd20c4e52636abc1ae0e8cfdfe6241d83408cd
tearight <tearightss@gmail.com>
2020-12-21 20:27:06 +0900

    feat: 기본적인 학습테스트를 위한 Station, StationRepository 생성

---
+++ src/main/java/jpa/domain/Station.java
@@ -0,0 +1,34 @@
+package jpa.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "station")
+public class Station {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false, unique = true, length = 255)
+	private String name;
+
+	public Station() {
+	}
+
+	Station(String name) {
+		this.name = name;
+	}
+
+	Long getId() {
+		return id;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+}
