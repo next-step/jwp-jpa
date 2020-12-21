993c24d0e8a6845ae9cad143a23d360876997709
wehwan <we_hwan@naver.com>
2020-12-21 10:45:58 +0900

    feat: Member entity 및 repository 추가

---
+++ src/main/java/jpa/domain/Member.java
@@ -0,0 +1,71 @@
+package jpa.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    private Date created_date;
+
+    private Date modified_date;
+
+    private Integer age;
+
+    private String email;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public Date getModified_date() {
+        return modified_date;
+    }
+
+    public void setModified_date(Date modified_date) {
+        this.modified_date = modified_date;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/jpa/domain/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package jpa.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("member 저장 테스트")
+    public void save() {
+        // given
+        Member member = new Member();
+
+        // when
+        Member saveMember = this.memberRepository.save(member);
+
+        // then
+        assertThat(saveMember).isEqualTo(member);
+    }
+
+    @Test
+    @DisplayName("이름으로 member 조회 테스트")
+    public void findByName() {
+        // given
+        Member member = memberRepository.save(new Member());
+        Long memberId = member.getId();
+
+        // when
+        Optional<Member> findMember = this.memberRepository.findById(memberId);
+
+        // then
+        assertThat(findMember.get().getId()).isEqualTo(memberId);
+    }
+}
