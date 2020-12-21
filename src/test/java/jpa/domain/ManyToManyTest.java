8cf80d804db3edab9e45b7fc7a13a2a936385234
poppohoo <ccf05017@gmail.com>
2020-12-21 21:03:14 +0900

    test(ManyToManyTest): dirty check test
    - 영속화 된 엔티티의 더티체킹을 통한 자동 업데이트 기능 확인

--- src/test/java/jpa/domain/ManyToManyTest.java
+++ src/test/java/jpa/domain/ManyToManyTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 @DataJpaTest
 public class ManyToManyTest {
@@ -23,16 +22,15 @@ public class ManyToManyTest {
     @DisplayName("Line에서 새로운 Station을 등록하고 저장할 수 있다.")
     @Test
     void addStationTest() {
-        // given
-        ManyToManyStation station = new ManyToManyStation("gangnam");
-        ManyToManyLine line = new ManyToManyLine("green", "lineNumber2");
-        line.addStation(station);
+        String stationName = "gangnam";
+        String lineName = "lineNumber2";
+        String lineColor = "green";
 
         // when
-        lineRepository.save(line);
+        LINE_SAVED_WITH_STATION(stationName, lineName, lineColor);
 
         // then
-        ManyToManyStation foundStation = stationRepository.findByName(station.getName()).orElse(null);
+        ManyToManyStation foundStation = stationRepository.findByName(stationName).orElse(null);
         assertThat(foundStation).isNotNull();
     }
 
@@ -43,17 +41,14 @@ public class ManyToManyTest {
         String lineColor = "green";
         String lineName = "lineNumber2";
         // given
-        ManyToManyStation station = new ManyToManyStation(stationName);
-        ManyToManyLine line = new ManyToManyLine(lineName, lineColor);
-        line.addStation(station);
-        lineRepository.save(line);
+        LINE_SAVED_WITH_STATION(stationName, lineName, lineColor);
 
         // when
-        ManyToManyLine foundLine = lineRepository.findByName(line.getName()).orElse(null);
+        ManyToManyLine foundLine = lineRepository.findByName(lineName).orElse(null);
         assertThat(foundLine).isNotNull();
 
         // then
-        assertThat(foundLine.getStations()).contains(station);
+        assertThat(foundLine.getStations().get(0).getName()).contains(stationName);
     }
 
     @DisplayName("Staiton에서 Line을 조회할 수 있다.")
@@ -63,10 +58,7 @@ public class ManyToManyTest {
         String lineColor = "green";
         String lineName = "lineNumber2";
         // given
-        ManyToManyStation station = new ManyToManyStation(stationName);
-        ManyToManyLine line = new ManyToManyLine(lineName, lineColor);
-        line.addStation(station);
-        lineRepository.save(line);
+        ManyToManyLine line = LINE_SAVED_WITH_STATION(stationName, lineName, lineColor);
 
         // when
         ManyToManyStation foundStation = stationRepository.findByName(stationName).orElse(null);
@@ -74,5 +66,35 @@ public class ManyToManyTest {
 
         // then
         assertThat(foundStation.getLines()).contains(line);
+    }
+
+    @DisplayName("더티 체킹을 통해 영속화 된 Line이 자동으로 업데이트 된다.")
+    @Test
+    void dirtyCheckTest() {
+        String stationName = "uiwang";
+        String lineColor = "blue";
+        String lineName = "linNumber1";
+        int expectedSize = 2;
+
+        // given
+        ManyToManyLine line = LINE_SAVED_WITH_STATION(stationName, lineName, lineColor);
+
+        // when
+        line.addStation(new ManyToManyStation("sindorim"));
+
+        // then
+        ManyToManyLine foundLine = lineRepository.findByName(lineName).orElse(null);
+        assertThat(foundLine).isNotNull();
+        assertThat(foundLine.getStations()).hasSize(expectedSize);
+    }
+
+    private ManyToManyLine LINE_SAVED_WITH_STATION(
+            final String stationName, final String lineName, final String lineColor
+    ) {
+        ManyToManyStation station = new ManyToManyStation(stationName);
+        ManyToManyLine line = new ManyToManyLine(lineName, lineColor);
+        line.addStation(station);
+
+        return lineRepository.save(line);
     }
 }
