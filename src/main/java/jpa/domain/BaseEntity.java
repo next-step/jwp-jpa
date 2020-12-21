70d2dd7e8644b0d7b5ce2c36e5062ac7ce896114
shk3029 <shk30290@naver.com>
2020-12-21 11:26:31 +0900

    refactor: GeneratedValue strategy AUTO로 수정

--- src/main/java/jpa/domain/BaseEntity.java
+++ src/main/java/jpa/domain/BaseEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @CreatedDate
