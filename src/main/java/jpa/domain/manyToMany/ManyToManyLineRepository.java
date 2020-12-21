4b1b731c15ae6146d39ccef95fbb22f2f012214c
poppohoo <ccf05017@gmail.com>
2020-12-21 23:06:33 +0900

    refactor(ManyToMany): remove useless
    - 요구사항을 잘못 이해하고 구현한 잘못된 구성요소들 모두 제거

--- src/main/java/jpa/domain/manyToMany/ManyToManyLineRepository.java
+++
@@ -1,9 +0,0 @@
-package jpa.domain.manyToMany;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ManyToManyLineRepository extends JpaRepository<ManyToManyLine, Long> {
-    Optional<ManyToManyLine> findByName(String name);
-}
--- src/main/java/jpa/domain/manyToMany/ManyToManyStationRepository.java
+++
@@ -1,9 +0,0 @@
-package jpa.domain.manyToMany;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ManyToManyStationRepository extends JpaRepository<ManyToManyStation, Long> {
-    Optional<ManyToManyStation> findByName(String name);
-}
