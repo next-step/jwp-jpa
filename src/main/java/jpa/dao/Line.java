23a5a7952fce5874f1c41a6960642c2fb5451b23
lazy-moon <magyeon@naver.com>
2020-12-22 03:49:51 +0900

    feature: Line, Station의 연관관계 수정

--- src/main/java/jpa/dao/Line.java
+++ src/main/java/jpa/dao/Line.java
@@ -1,13 +1,18 @@
 package jpa.dao;
 
-import org.springframework.validation.annotation.Validated;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Line extends DefaultEntity {
+    @Column(unique = true)
     private String name;
     private String color;
+    @OneToMany(mappedBy = "line")
+    private List<Distance> distances = new ArrayList<>();
 
     protected Line(){}
 
@@ -22,6 +27,10 @@ public class Line extends DefaultEntity {
 
     public String getName() {
         return name;
+    }
+
+    public List<Distance> getDistances() {
+        return distances;
     }
 
     public void updateName(String name) {
