798970ca2cd53c8fdf8082eddc0f6f3dbf67289a
shk3029 <shk30290@naver.com>
2020-12-21 11:37:31 +0900

    feat: EnableJpaAuditing 기능을 수행하는 confing 추ㅏㄱ

--- src/main/java/jpa/Application.java
+++ src/main/java/jpa/Application.java
@@ -2,9 +2,7 @@ package jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "commonAuditorAware")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
