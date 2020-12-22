3450005597a2504462243323734582cf80751818
louisJu <rokmctkd6@gmail.com>
2020-12-22 17:05:21 +0900

    feat: step3 다대다 연관관계 리팩터링 * station, line 매핑시 거리 값 추가, test 진행.

--- src/main/java/jpa/entity/StationLine.java
+++ src/main/java/jpa/entity/StationLine.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.util.Objects;
 
 
 @Getter
@@ -23,4 +24,25 @@ public class StationLine extends BaseEntity {
     @JoinColumn(name = "line_id")
     private Line line;
 
+    private int distance;
+
+    public StationLine(Station station, Line line, int distance) {
+        this.station = station;
+        this.line = line;
+        this.distance = distance;
+    }
+
+    public void changeStationLine(Station station, Line line, int distance) {
+        if(Objects.nonNull(this.station)
+                && Objects.nonNull(this.line)
+                && Objects.nonNull(this.station)) {
+            this.station.getStationLines().remove(this);
+            this.line.getStationLines().remove(this);
+        }
+        this.station = station;
+        this.line = line;
+        this.distance = distance;
+        station.getStationLines().add(this);
+        line.getStationLines().add(this);
+    }
 }
