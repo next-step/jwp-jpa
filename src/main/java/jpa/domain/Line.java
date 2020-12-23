b558650039d759fde2955fa27a2f0911289829ac
wehwan <we_hwan@naver.com>
2020-12-23 20:14:19 +0900

    feat: Line, Station 엔티티 연관관계 매핑

--- src/main/java/jpa/domain/Line.java
+++ src/main/java/jpa/domain/Line.java
@@ -8,6 +8,9 @@ import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "line")
@@ -21,6 +24,12 @@ public class Line extends JpaAuditingDate {
 
     @Column(unique = true)
     private String name;
+
+    @ManyToMany
+    @JoinTable(name = "station_line"
+                , joinColumns = @JoinColumn(name = "line_id")
+                , inverseJoinColumns = @JoinColumn(name = "station_id"))
+    private List<Station> stations = new ArrayList<>();
 
     protected Line() {
     }
@@ -39,5 +48,39 @@ public class Line extends JpaAuditingDate {
 
     public String getName() {
         return this.name;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void addStation(Station station) {
+        this.stations.add(station);
+        station.addLine(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(id, line.id) &&
+                Objects.equals(color, line.color) &&
+                Objects.equals(name, line.name) &&
+                Objects.equals(stations, line.stations);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, color, name, stations);
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/main/java/jpa/domain/Station.java
+++ src/main/java/jpa/domain/Station.java
@@ -8,6 +8,9 @@ import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "station")
@@ -19,6 +22,9 @@ public class Station extends JpaAuditingDate {
 
     @Column(unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "stations")
+    private List<Line> lines = new ArrayList<>();
 
     protected Station() {
     }
@@ -33,5 +39,35 @@ public class Station extends JpaAuditingDate {
 
     public String getName() {
         return this.name;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+
+    public void addLine(Line line) {
+        this.lines.add(line);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Station station = (Station) o;
+        return Objects.equals(id, station.id) &&
+                Objects.equals(name, station.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Station{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
