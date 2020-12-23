f47156a8721cccc8db86567cea5f2f93c89af952
orgojy <giveawesome@gmail.com>
2020-12-23 21:38:19 +0900

    feat: `Line` 엔티티 매핑 추가

---
+++ src/main/java/jpa/domain/LineRepository.java
@@ -0,0 +1,9 @@
+package jpa.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LineRepository extends JpaRepository<Line, Long> {
+    Line findByName(String name);
+}
