932e439d52a1724cfcb8587892e2bc390ed63d61
bss <baekss0409@gmail.com>
2020-12-24 00:08:42 +0900

    test: Station 테스트

---
+++ src/main/java/jpa/domain/station/StationRepository.java
@@ -0,0 +1,11 @@
+package jpa.domain.station;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StationRepository extends JpaRepository<Station, Long> {
+    Station findByName(String name);
+
+    @EntityGraph(attributePaths = "lines")
+    Station findLineEntityGraphByName(String name);
+}
--- src/main/java/jpa/repository/StationRepository.java
+++
@@ -1,9 +0,0 @@
-package jpa.repository;
-
-import jpa.domain.Station;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StationRepository extends JpaRepository<Station, Long> {
-    Station findByName(String name);
-}
