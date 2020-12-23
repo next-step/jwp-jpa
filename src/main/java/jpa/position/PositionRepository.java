4f23f0305463dc7bb2125eff5584f262238cbe4c
charlloss.dev <charlloss.dev@gmail.com>
2020-12-23 22:03:27 +0900

    test: position - station, line 양방향 조인관계 생성 및 데이터 테스트

--- src/main/java/jpa/position/PositionRepository.java
+++ src/main/java/jpa/position/PositionRepository.java
@@ -1,6 +1,15 @@
 package jpa.position;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jpa.line.Line;
+import jpa.station.Station;
+
 public interface PositionRepository extends JpaRepository<Position, Long> {
+
+	List<Position> findByLine(Line line);
+
+	List<Position> findByStation(Station station);
 }
--- src/test/java/jpa/position/PositionTest.java
+++ src/test/java/jpa/position/PositionTest.java
@@ -33,8 +33,6 @@ class PositionTest {
 
 	private Line lineNumber1;
 	private Line lineNumber2;
-	private Line lineNumber3;
-	private Line lineNumber5;
 
 	@BeforeEach
 	void init() {
@@ -44,8 +42,6 @@ class PositionTest {
 
 		lineNumber1 = lineRepository.save(new Line("1호선", Color.BLUE));
 		lineNumber2 = lineRepository.save(new Line("2호선", Color.GREEN));
-		lineNumber3 = lineRepository.save(new Line("3호선", Color.ORANGE));
-		lineNumber5 = lineRepository.save(new Line("5호선", Color.MAGENTA));
 
 		lineNumber1.addStation(시청);
 		lineNumber1.addStation(서울);
@@ -57,30 +53,67 @@ class PositionTest {
 
 	@Test
 	@DisplayName("위치 엔티티 생성 테스트")
-	void createPositionTest() {
-
-		Position pLine2시청 = positionRepository.save(new Position(78));
-		Position pLine1시청 = positionRepository.save(new Position(20));
-
-		Position pLine1서울 = positionRepository.save(new Position(12));
-		Position pLine1용산 = positionRepository.save(new Position(5));
-
-		assertThat(positionRepository.findAll()).hasSize(4);
-		System.out.println(positionRepository.findAll());
-	}
-
-	@Test
-	@DisplayName("위치 엔티티 생성 테스트")
 	void joinPositionTest() {
 
 		Position pLine1시청 = positionRepository.save(new Position(20));
 		Position pLine2시청 = positionRepository.save(new Position(78));
-
-		Position pLine1서울 = positionRepository.save(new Position(12));
-		Position pLine1용산 = positionRepository.save(new Position(5));
-
-		assertThat(positionRepository.findAll()).hasSize(4);
-		System.out.println(positionRepository.findAll());
 	}
 
+	@Test
+	@DisplayName("양방향: 위치 엔티티 양방향 거리 추가 테스트")
+	void addLocationTest() {
+
+		Position pLine1시청 = positionRepository.save(new Position(20));
+		Position pLine2시청 = positionRepository.save(new Position(78));
+
+		pLine1시청.addSubway(시청, lineNumber1);
+		pLine2시청.addSubway(시청, lineNumber2);
+
+		assertThat(positionRepository.findAll()).hasSize(2);
+		assertThat(positionRepository.findByLine(lineNumber1)).hasSize(1);
+		assertThat(positionRepository.findByStation(시청)).hasSize(2);
+
+		assertThat(lineNumber1.getPositions()).hasSize(1);
+		assertThat(lineNumber2.getPositions()).hasSize(1);
+	}
+
+	@Test
+	@DisplayName("양방향: Station 엔티티로 위치값 가져오기 테스트")
+	void getLocationToStationTest() {
+		Position pLine1시청 = positionRepository.save(new Position(20));
+		Position pLine2시청 = positionRepository.save(new Position(78));
+
+		pLine1시청.addSubway(시청, lineNumber1);
+		pLine2시청.addSubway(시청, lineNumber2);
+
+		Station actual = stationRepository.findByName("시청");
+		//assertThat(positionRepository.findByStation(actual)).hasSize(2);
+
+		assertThat(actual.getPositions()).hasSize(2);
+		assertThat(actual.getLocation(lineNumber1)).isEqualTo(20);
+		assertThat(actual.getLocation(lineNumber2)).isEqualTo(78);
+	}
+
+	@Test
+	@DisplayName("양방향: Line 엔티티로 위치값 가져오기 테스트")
+	void getLocationToLintest() {
+		Position pLine1용산 = positionRepository.save(new Position(50));
+		Position pLine1서울 = positionRepository.save(new Position(57));
+		Position pLine1시청 = positionRepository.save(new Position(65));
+
+		pLine1용산.addSubway(용산, lineNumber1);
+		pLine1서울.addSubway(서울, lineNumber1);
+		pLine1시청.addSubway(시청, lineNumber1);
+
+		Line actual = lineRepository.findByName("1호선");
+
+		assertThat(positionRepository.findByLine(actual)).hasSize(3);
+		assertThat(actual.getPositions()).hasSize(3);
+
+		assertThat(actual.getLocation(용산)).isEqualTo(50);
+		assertThat(actual.getLocation(서울)).isEqualTo(57);
+		assertThat(actual.getLocation(시청)).isEqualTo(65);
+	}
 }
+
+
