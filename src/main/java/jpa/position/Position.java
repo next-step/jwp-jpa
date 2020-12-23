ddf20525242cb20a7d5bba5560bb42d28224617c
charlloss.dev <charlloss.dev@gmail.com>
2020-12-23 22:02:39 +0900

    feat: position - station, line 양방향 조인관계 구현

--- src/main/java/jpa/position/Position.java
+++ src/main/java/jpa/position/Position.java
@@ -5,8 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import jpa.common.BaseTime;
+import jpa.line.Line;
+import jpa.station.Station;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
@@ -17,6 +22,17 @@ public class Position extends BaseTime {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Getter
+	@ManyToOne
+	@JoinColumn
+	private Line line;
+
+	@Getter
+	@ManyToOne
+	@JoinColumn
+	private Station station;
+
+	@Getter
 	@Column(nullable = false)
 	private int location;
 
@@ -27,4 +43,11 @@ public class Position extends BaseTime {
 		this.location = location;
 	}
 
+	public void addSubway(Station station, Line line) {
+		station.getPositions().add(this);
+		line.getPositions().add(this);
+
+		this.station = station;
+		this.line = line;
+	}
 }
