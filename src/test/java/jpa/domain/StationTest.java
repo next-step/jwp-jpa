fbf27e096e1179a2239a6d86c1c6b3f13fb27f96
charllossdev <charlloss.dev@gmail.com>
2020-12-23 15:42:03 +0900

    test: 양방향 연결 편의메서드 테스트

--- src/test/java/jpa/domain/StationTest.java
+++ src/test/java/jpa/domain/StationTest.java
@@ -4,9 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.awt.*;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -34,122 +31,9 @@ class StationTest {
 	@BeforeEach
 	void init() {
 		line = lineRepository.save(new Line(Color.ORANGE, "3호선"));
-		station = stationRepository.save(new Station("교대역", Collections.singletonList(line)));
-		lineRepository.flush();
-	}
-
-	@Test
-	@DisplayName("영속성 컨텍스트: 엔티티 객체 생성 및 동일성 보장 테스트")
-	void entitySaveTest() {
-		Station expected = stationRepository.save(new Station("시청역"));
-		Station actual = stationRepository.findById(expected.getId()).get();
-
-		assertThat(actual).isSameAs(expected);
-	}
-
-	@Test
-	@DisplayName("영속성 컨텍스트: 변경 감지 테스트")
-	void entityChangeTest() {
-		Station expected = stationRepository.save(new Station("잠실역"));
-		expected.changeName("시청역");
-
-		Station actual = stationRepository.findByName("시청역");
-
-		assertThat(actual).isNotNull();
-		assertThat(actual).isSameAs(expected);
-	}
-
-	@Test
-	@DisplayName("영속성 컨텍스트: 변경 감지, 키 조회 변경 테스트")
-	void entityChangeKeyTest() {
-		Station expected = stationRepository.save(new Station("잠실역"));
-		expected.changeId(3L);
-
-		assertThatExceptionOfType(NoSuchElementException.class)
-			.isThrownBy(() -> {
-					stationRepository.findById(expected.getId());
-				}
-			);
-	}
-
-	@Test
-	@DisplayName("연관 관계: 연관 관계 엔티티 정보 가져오기 테스트")
-	void getMappingTest() {
-		final Station expected = new Station("잠실역", Collections.singletonList(line));
-
-		Station actual = stationRepository.save(expected);
-		System.out.println(actual);
-		assertThat(actual.getFirstLine().getName()).isEqualTo("3호선");
-	}
-
-	@Test
-	@DisplayName("연관 관계: 연관 관계 엔티티 설정 및 저장 에러 테스트")
-	void saveErrorWithLineTest() {
-		final Station expected = new Station("잠실역");
-		expected.setLines(Collections.singletonList(new Line(Color.GREEN, "2호선"))); // 생성한 Line 이 영속 상태가 아니기 떄문에
-		stationRepository.save(expected);
-		final Station actual = stationRepository.findByName("잠실역");
-		assertThat(actual).isNotNull();
-		assertThat(expected).isSameAs(actual);
-		assertThat(actual.getFirstLine()).isNull();
-	}
-
-	@Test
-	@DisplayName("연관 관계: 연관 맵핑 엔티티 정보 가져오기 테스트")
-	void findByNameWithLine() {
-		Station actual = stationRepository.findByName("교대역");
-
-		assertThat(actual).isNotNull();
-		assertThat(actual.getFirstLine().getName()).isEqualTo("3호선");
-	}
-
-	@Test
-	@DisplayName("연관 관계: 맵핑 엔티티 정보 수정 테스트")
-	void updateMappingEntityTest() {
-		Station expected = stationRepository.findByName("교대역");
-
-		expected.setLines(Arrays.asList(
-			lineRepository.save(new Line(Color.GREEN, "2호선")),
-			line
-		));
-		stationRepository.flush();
-		// stationRepository.save(expected); // Error
-
-		assertThat(expected.getFirstLine().getName()).isEqualTo("2호선");
-		assertThat(expected.getLines().size()).isEqualTo(2);
-	}
-
-	@Test
-	@DisplayName("연관 관계: 맵핑 엔티티 정보 삭제 테스트")
-	void deleteMappingEntityTest() {
-		Station expected = stationRepository.findByName("교대역");
-
-		expected.setLines(null);
-		stationRepository.flush();
-		// stationRepository.save(expected); // Error
-
-		assertThat(expected.getLines()).isNull();
-	}
-
-	@Test
-	@DisplayName("양방향 연관 관계: 다 대 다(ManyToMany) 양방향 연관 관계, 맵핑된 엔티티 클래스 양방향 조회 장애 테스트")
-	void selectJoinErrorEntityTest() {
-		final Line lineNumber3 = lineRepository.findByName("3호선");
-
-		assertThat(lineNumber3.getStations().size()).isEqualTo(0);
-	}
-
-	@Test
-	@DisplayName("양방향 연관 관계: 다 대 다(ManyToMany) 양방향 업데이트 확인 테스트")
-	void selectJoinEntityTest() {
-		final Line lineNumber3 = lineRepository.findByName("3호선");
-		final Station expected = new Station("양재역");
-
-		stationRepository.save(expected);
-		lineNumber3.getStations().add(expected);
-		stationRepository.flush();
-
-		assertThat(lineNumber3.getStations()).hasSize(1);
+		station = new Station("교대역");
+		station.addLine(line);
+		stationRepository.save(station);
 	}
 
 	@Test
@@ -168,5 +52,118 @@ class StationTest {
 
 		assertThat(actual.getCreatedDate()).isAfter(now);
 		assertThat(actual.getModifiedDate()).isAfter(now);
+	}
+
+	@Test
+	@DisplayName("양방향 연관 관계: 연관관계 편의 메서드 생성")
+	void functionEntityTest() {
+		final Line lineNumber3 = lineRepository.findByName("3호선");
+		final Station expected = new Station("양재역");
+		// 1대 다 단방향, 쿼리 생성 확인
+		assertThat(lineNumber3.getStations()).hasSize(1);
+
+		stationRepository.save(expected);
+		lineNumber3.addStation(expected);  // 연관관계 편의 메서드
+		stationRepository.flush();
+
+		assertThat(lineNumber3.getStations()).hasSize(2);
+	}
+
+	@Test
+	@DisplayName("양방향 연결편의 메서드: Station Add -> Line 연결편의 메서드 추가 연결 테스트")
+	void stationAddLineTest() {
+		final Station expected = stationRepository.save(new Station("종로3가"));
+		final Line lineNumber1 = lineRepository.save(new Line(Color.BLUE, "1호선"));
+		final Line lineNumber5 = lineRepository.save(new Line(Color.MAGENTA, "5호선"));
+
+		expected.addLine(lineNumber1);
+		expected.addLine(lineNumber5);
+		final Station actual = stationRepository.findByName("종로3가");
+		final Line actualLine1 = lineRepository.findByName("1호선");
+		final Line actualLine5 = lineRepository.findByName("5호선");
+
+		assertThat(actual).isNotNull();
+		assertThat(actual.getLines()).hasSize(2);
+		assertThat(actualLine1.getStations()).hasSize(1);
+		assertThat(actualLine5.getStations()).hasSize(1);
+	}
+
+	@Test
+	@DisplayName("양방향 연결편의 메서드: Line Add -> Station 연결편의 메서드 추가 연결 테스트")
+	void lineAddStationTest() {
+		final Station expected = stationRepository.save(new Station("종로3가"));
+		final Line lineNumber1 = lineRepository.save(new Line(Color.BLUE, "1호선"));
+		final Line lineNumber5 = lineRepository.save(new Line(Color.MAGENTA, "5호선"));
+
+		lineNumber1.addStation(expected);
+		lineNumber5.addStation(expected);
+
+		final Station actual = stationRepository.findByName("종로3가");
+		final Line actualLine1 = lineRepository.findByName("1호선");
+		final Line actualLine5 = lineRepository.findByName("5호선");
+
+		assertThat(actual).isNotNull();
+		assertThat(actual.getLines()).hasSize(2);
+		assertThat(actualLine1.getStations()).hasSize(1);
+		assertThat(actualLine5.getStations()).hasSize(1);
+	}
+
+	@Test
+	@DisplayName("양방향 연결편의 메서드: 연결편의 메서드 추가 연결 테스트")
+	void stationAndLineTest() {
+		final Line newLine = lineRepository.save(new Line(Color.GREEN, "2호선"));
+		station.addLine(newLine);
+
+		assertThat(station.getLines()).hasSize(2); // 2호선, 3호선
+		assertThat(newLine.getStations()).hasSize(1); // 교대역
+
+		final Station newStation = stationRepository.save(new Station("강남역"));
+		newLine.addStation(newStation);
+
+		assertThat(newLine.getStations()).hasSize(2); // 교대역, 강남역
+	}
+
+	@Test
+	@DisplayName("양방향 연결편의 메서드: Station clear -> Line 테스트")
+	void stationClearLineTest() {
+		final Station expected = stationRepository.save(new Station("종로3가"));
+		final Line lineNumber1 = lineRepository.save(new Line(Color.BLUE, "1호선"));
+		final Line lineNumber5 = lineRepository.save(new Line(Color.MAGENTA, "5호선"));
+
+		lineNumber1.addStation(expected);
+		lineNumber5.addStation(expected);
+		stationRepository.flush();
+
+		expected.clearLine(lineNumber1);
+
+		final Station actual = stationRepository.findByName("종로3가");
+		final Line actualLine1 = lineRepository.findByName("1호선");
+		final Line actualLine5 = lineRepository.findByName("5호선");
+
+		assertThat(actual.getLines()).hasSize(1);
+		assertThat(actualLine1.getStations()).isEmpty();
+		assertThat(actualLine5.getStations()).hasSize(1);
+	}
+
+	@Test
+	@DisplayName("양방향 연결편의 메서드: Line clear -> Station 테스트")
+	void lineClearStationTest() {
+		final Station expected = stationRepository.save(new Station("종로3가"));
+		final Line lineNumber1 = lineRepository.save(new Line(Color.BLUE, "1호선"));
+		final Line lineNumber5 = lineRepository.save(new Line(Color.MAGENTA, "5호선"));
+
+		lineNumber1.addStation(expected);
+		lineNumber5.addStation(expected);
+		stationRepository.flush();
+
+		lineNumber1.clearStation(expected);
+
+		final Station actual = stationRepository.findByName("종로3가");
+		final Line actualLine1 = lineRepository.findByName("1호선");
+		final Line actualLine5 = lineRepository.findByName("5호선");
+
+		assertThat(actual.getLines()).hasSize(1);
+		assertThat(actualLine1.getStations()).isEmpty();
+		assertThat(actualLine5.getStations()).hasSize(1);
 	}
 }
