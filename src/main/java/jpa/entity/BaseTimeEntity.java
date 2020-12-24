140b5fa0ec2f11a08ee95e17492cd97d4a783c86
chh <chh2644@gmail.com>
2020-12-24 23:52:57 +0900

    feat : 기본 엔티티 생성

---
+++ src/main/java/jpa/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package jpa.entity;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreationTimestamp
+    private LocalDateTime created_date;
+
+    @UpdateTimestamp
+    private LocalDateTime updated_date;
+}
