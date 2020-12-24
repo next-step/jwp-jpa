1077946636c0f42dcfe3dd95a875192c4a94eebb
chh <chh2644@gmail.com>
2020-12-24 23:51:09 +0900

    feat : 기초 데이터 셋팅

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO line (id, name) VALUES (1, '3호선')
-INSERT INTO station (id, line_id, name) VALUES (1, 1, '교대역')
+INSERT INTO line (id, name, color) VALUES (1, '2호선', 'green')
+INSERT INTO station (id, name) VALUES (1, '당산역')
+INSERT INTO station_line (station_id, line_id) VALUES (1, 1)
+INSERT INTO member (id, age, email, password) VALUES (1, 20, 'chh9975@naver.com', '1234')
