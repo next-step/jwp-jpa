20dfee6c576f1b48fb1fbe531b46fe4a9c56a28e
shk3029 <shk30290@naver.com>
2020-12-24 17:06:59 +0900

    test: StationLineRepositoryTest

--- src/test/java/jpa/repository/StationLineRepositoryTest.java
+++ src/test/java/jpa/repository/StationLineRepositoryTest.java
@@ -1,5 +1,8 @@
 package jpa.repository;
 
+import jpa.domain.Line;
+import jpa.domain.Station;
+import jpa.domain.StationLine;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,11 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StationLineRepositoryTest {
 
     @Autowired
+    private StationRepository stationRepository;
+
+    @Autowired
+    private LineRepository lineRepository;
+
+    @Autowired
     private StationLineRepository stationLineRepository;
 
     @Test
     void save() {
-        
+        Station station = stationRepository.save(new Station("사당역"));
+        Line line = lineRepository.save(new Line("2호선"));
+
+        StationLine stationLine = new StationLine(station,line);
+        StationLine actual = stationLineRepository.save(stationLine);
+
+        assertEquals(stationLine, actual);
     }
 
 
