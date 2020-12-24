ad65c68dc6e513e6b75a5d46fd4da738999eb518
shk3029 <shk30290@naver.com>
2020-12-24 17:07:50 +0900

    test: @DataJpaTest 추가

--- src/test/java/jpa/repository/StationLineRepositoryTest.java
+++ src/test/java/jpa/repository/StationLineRepositoryTest.java
@@ -5,9 +5,11 @@ import jpa.domain.Station;
 import jpa.domain.StationLine;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class StationLineRepositoryTest {
 
     @Autowired
