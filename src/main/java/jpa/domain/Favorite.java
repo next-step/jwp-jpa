c54ec68ca266b24300f7075e00b00a5d3c84c69e
shk3029 <shk30290@naver.com>
2020-12-24 16:50:37 +0900

    feat: 사용자가 즐겨찾기를 조회하는 기능 추가

--- src/main/java/jpa/domain/Favorite.java
+++ src/main/java/jpa/domain/Favorite.java
@@ -1,22 +1,23 @@
 package jpa.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import java.util.Objects;
 
+@Setter
 @Getter
 @Entity
 public class Favorite extends BaseEntity {
 
     @OneToOne
-    @JoinColumn(name = "station_id", insertable = false, updatable = false)
     private Station startStation;
 
     @OneToOne
-    @JoinColumn(name = "station_id", insertable = false, updatable = false)
     private Station endStation;
 
     @ManyToOne
@@ -24,5 +25,33 @@ public class Favorite extends BaseEntity {
     private Member member;
 
     public Favorite() {
+    }
+
+    public Favorite(Station startStation, Station endStation, Member member) {
+        this.startStation = startStation;
+        this.endStation = endStation;
+        this.member = member;
+        member.addFavorites(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Favorite favorite = (Favorite) o;
+        return Objects.equals(startStation, favorite.startStation) &&
+                Objects.equals(endStation, favorite.endStation) &&
+                Objects.equals(member, favorite.member);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startStation, endStation, member);
+    }
+
+    @Override
+    public String toString() {
+        return "startStation=" + startStation +
+                ", endStation=" + endStation;
     }
 }
--- src/main/java/jpa/domain/Member.java
+++ src/main/java/jpa/domain/Member.java
@@ -4,20 +4,53 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Setter
 @Getter
 @Entity
 public class Member extends BaseEntity {
 
+    private String name;
     private String age;
     private String email;
     private String password;
 
     @OneToMany(mappedBy = "member")
     private List<Favorite> favorites = new ArrayList<>();
+
+    public void addFavorites(Favorite favorite) {
+        favorites.add(favorite);
+        favorite.setMember(this);
+    }
+
+    public Member() {
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(name, member.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
