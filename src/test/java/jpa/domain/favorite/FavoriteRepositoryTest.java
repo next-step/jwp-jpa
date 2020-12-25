c737ee96b071486b9121b22998e5186f4b80932e
mkkim90 <mkzzang0928@gmail.com>
2020-12-25 13:50:18 +0900

    test : 멤버 및 즐겨찾기 테스트

--- src/test/java/jpa/domain/favorite/FavoriteRepositoryTest.java
+++ src/test/java/jpa/domain/favorite/FavoriteRepositoryTest.java
@@ -1,5 +1,7 @@
 package jpa.domain.favorite;
 
+import jpa.domain.member.Member;
+import jpa.domain.member.MemberRepository;
 import jpa.domain.station.Station;
 import jpa.domain.station.StationRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -7,15 +9,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class FavoriteRepositoryTest{
+    private static final String ARRIVAL_STATION_NAME = "arrivalStationName";
+    private static final String DEPARTURE_STATION_NAME = "departureStationName";
+    private static final String MEMBER_NAME = "memberName";
+
     @Autowired
     FavoriteRepository favorites;
 
     @Autowired
     StationRepository stations;
+
+    @Autowired
+    MemberRepository members;
 
     @Test
     void save() {
@@ -25,16 +39,80 @@ public class FavoriteRepositoryTest{
     @DisplayName("출발역과 도착역 세팅")
     @Test
     void setDepartureStationAndsetArrivalStation() {
-        // given
-        Station departureStation = stations.save(new Station("천호역"));
-        Station arrivalStation = stations.save(new Station("여의도역"));
-
         // when
-        Favorite favorite = favorites.save(new Favorite(departureStation, arrivalStation));
-
+        Favorite favorite = setFavoriteWithDepartureStationAndArrivalStation(new String[]{"천호역", "여의도역"});
         // then
         assertThat(favorite.getDepartureStation()).isNotNull();
         assertThat(favorite.getArrivalStation()).isNotNull();
         assertThat(favorite.getMember()).isNull();
+    }
+
+    private Favorite setFavoriteWithDepartureStationAndArrivalStation(String[] params) {
+        Station departureStation = stations.save(new Station(params[0]));
+        Station arrivalStation = stations.save(new Station(params[1]));
+
+        Favorite favorite = favorites.save(new Favorite(departureStation, arrivalStation));
+        return favorite;
+    }
+
+    @DisplayName("즐겨찾기 업데이트")
+    @Test
+    void update() {
+        // then
+        Map<String, String> param = new HashMap<String, String>();
+        param.put(MEMBER_NAME, "김민균");
+        param.put(ARRIVAL_STATION_NAME,"여의도역");
+        param.put(DEPARTURE_STATION_NAME, "천호역");
+        Favorite favorite = saveFavorite(param);
+
+        // when
+        Member member = members.save(new Member("포비"));
+        favorite.changeMember(member);
+        Favorite favorite1 = favorites.findByMember(member);
+
+        assertThat(favorite.getMember().getName()).isEqualTo(member.getName());
+    }
+
+    private Favorite saveFavorite(Map<String, String> param) {
+        Station departureStation = stations.save(new Station(param.get(DEPARTURE_STATION_NAME)));
+        Station arrivalStation = stations.save(new Station(param.get(ARRIVAL_STATION_NAME)));
+        Member member = members.save(new Member(param.get(MEMBER_NAME)));
+
+        Favorite favorite = favorites.save(new Favorite(departureStation, arrivalStation, member));
+        return favorite;
+    }
+
+
+    @DisplayName("즐겨찾기 삭제")
+    @Test
+    void delete() {
+        // given
+        Map<String, String> param = new HashMap<String, String>();
+        param.put(MEMBER_NAME, "김민균");
+        param.put(ARRIVAL_STATION_NAME,"여의도역");
+        param.put(DEPARTURE_STATION_NAME, "천호역");
+        Favorite favorite = saveFavorite(param);
+
+        // when
+        favorites.delete(favorite);
+        favorites.flush();
+
+        // then
+        assertThat(favorites.findAll()).hasSize(0);
+        assertThat(members.findAll()).hasSize(1);
+        assertThat(stations.findAll()).hasSize(2);
+    }
+
+    @DisplayName("즐겨찾기 가져오기")
+    @Test
+    void find() {
+        Map<String, String> param = new HashMap<String, String>();
+        param.put(MEMBER_NAME, "김민균12");
+        param.put(ARRIVAL_STATION_NAME,"잠실역");
+        param.put(DEPARTURE_STATION_NAME, "천호역");
+        Favorite favorite = saveFavorite(param);
+
+        assertThat(favorites.findById(1L).get()).isEqualTo(favorite);
+        assertThat(favorites.findById(1L).get()).isEqualTo(favorite);
     }
 }
--- src/test/java/jpa/domain/member/MemberRepositoryTest.java
+++ src/test/java/jpa/domain/member/MemberRepositoryTest.java
@@ -1,6 +1,9 @@
 package jpa.domain.member;
 
+import jpa.domain.favorite.Favorite;
 import jpa.domain.favorite.FavoriteRepository;
+import jpa.domain.station.Station;
+import jpa.domain.station.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +16,12 @@ public class MemberRepositoryTest {
     @Autowired
     MemberRepository members;
 
+    @Autowired
+    StationRepository stations;
+
+    @Autowired
+    FavoriteRepository favorites;
+
     @Test
     void save() {
         Member expected = new Member("jason");
@@ -21,5 +30,38 @@ public class MemberRepositoryTest {
                 () -> assertThat(actual.getCreatedDate()).isNotNull(),
                 () -> assertThat(actual.getName()).isEqualTo(expected.getName())
         );
+    }
+
+    @Test
+    void update() {
+        // given
+        Member member = members.save(new Member("jason"));
+
+        // when
+        member.changeName("mkkim");
+        member.changeAge(32);
+        Favorite favorite = saveFavorite();
+        member.addFavorites(favorite);
+        members.flush();
+
+        // then
+        assertThat(members.findByName("mkkim").getAge()).isEqualTo(32);
+    }
+
+    private Favorite saveFavorite() {
+        Station departureStation = stations.save(new Station("강남역"));
+        Station arrivalStation = stations.save(new Station("잠실역"));
+        Favorite favorite = favorites.save(new Favorite(departureStation, arrivalStation));
+        return favorite;
+    }
+
+    @Test
+    void delete() {
+        Member member = members.save(new Member("mkkim"));
+        member.addFavorites(saveFavorite());
+        members.flush();
+
+        members.delete(member);
+        members.flush();
     }
 }
