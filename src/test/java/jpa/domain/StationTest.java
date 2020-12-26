5f6e07eb03d03613610d14ff92ac9aff1c09f6a6
Do Yeon Kim <chikeem90@gmail.com>
2020-12-26 19:29:23 +0900

    test: station 테스트 추가

---
+++ src/test/java/jpa/domain/StationTest.java
@@ -0,0 +1,45 @@
+package jpa.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import jpa.domain.station.Station;
+import jpa.domain.station.StationRepository;
+
+@DataJpaTest
+class StationTest {
+	@Autowired
+	StationRepository stations;
+
+	@Test
+	public void save() {
+		// given
+		Station expected = new Station("잠실역");
+
+		// when
+		Station actual = stations.save(expected);
+
+		// then
+		assertAll(
+			() -> assertThat(actual.getId()).isNotNull(),
+			() -> assertThat(actual.getName()).isEqualTo(expected.getName())
+		);
+	}
+
+	@Test
+	public void findByName() {
+		// given
+		String expected = "잠실역";
+
+		// when
+		stations.save(new Station("잠실역"));
+
+		// then
+		String actual = stations.findByName("잠실역").getName();
+		assertThat(expected).isEqualTo(actual);
+	}
+}
