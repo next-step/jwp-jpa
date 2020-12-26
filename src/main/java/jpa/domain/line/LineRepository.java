a4d96fa472c969ef53c8357a9e06e80009decd20
Do Yeon Kim <chikeem90@gmail.com>
2020-12-26 19:27:37 +0900

    refactor: 파라미터명 수정

--- src/main/java/jpa/domain/line/LineRepository.java
+++ src/main/java/jpa/domain/line/LineRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LineRepository extends JpaRepository<Line, Long> {
-	List<Line> findByColor(String expected);
+	List<Line> findByColor(String color);
 
-	Line findByName(String expected);
+	Line findByName(String name);
 }
