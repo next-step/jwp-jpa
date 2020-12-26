c5d6bc2606058442e9b2383557c9c23cbc5f96ce
Do Yeon Kim <chikeem90@gmail.com>
2020-12-26 19:45:14 +0900

    refactor: 어노테이션 추가

--- src/main/java/jpa/domain/BaseEntity.java
+++ src/main/java/jpa/domain/BaseEntity.java
@@ -2,9 +2,13 @@ package jpa.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 	private LocalDateTime create_date;
 	private LocalDateTime modified_date;
