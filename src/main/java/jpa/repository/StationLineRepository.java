4d93c100a41c630caf2487149d3be67a8afb4d4f
Jackcoding <36438257+shk3029@users.noreply.github.com>
2020-12-28 01:06:11 +0900

    [JPA] Step3 - 다대다 연관 관계 리팩터링 (#81)
    * docs: Step3 구현할 기능 설명 추가
    
    * feat: PreviousStation 추가
    
    * test: PreviousStation 추가

--- src/main/java/jpa/repository/StationLineRepository.java
+++ src/main/java/jpa/repository/StationLineRepository.java
@@ -1,8 +1,12 @@
 package jpa.repository;
 
+import jpa.domain.Line;
+import jpa.domain.Station;
 import jpa.domain.StationLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StationLineRepository extends JpaRepository<StationLine, Long> {
+import java.util.List;
 
+public interface StationLineRepository extends JpaRepository<StationLine, Long> {
+    StationLine findByStationAndLine(Station station, Line line);
 }
