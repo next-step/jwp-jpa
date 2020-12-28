09c75facd979d928fc60795619493873c7d782dd
Sang Uk Ahn <suahnn@gmail.com>
2020-12-28 15:01:47 +0900

    fix: 노선 조회 시 지하철 역 가져오는 테스트 수정

--- src/test/java/jpa/repository/LineRepositoryTest.java
+++ src/test/java/jpa/repository/LineRepositoryTest.java
@@ -2,15 +2,17 @@ package jpa.repository;
 
 import jpa.line.Line;
 import jpa.line.LineRepository;
+import jpa.route.Route;
 import jpa.station.Station;
 import jpa.station.StationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 public class LineRepositoryTest {
@@ -49,9 +51,10 @@ public class LineRepositoryTest {
         Line actual = lines.save(line);
 
         assertThat(actual.getRoutes()).hasSize(2);
-
-        actual.getRoutes()
-                .forEach(route ->
-                        assertTrue(route.getStation() == fromStation || route.getStation() == toStation));
+        assertThat(actual.getRoutes()
+                .stream()
+                .map(Route::getStation)
+                .collect(Collectors.toList()))
+                .contains(fromStation, toStation);
     }
 }
