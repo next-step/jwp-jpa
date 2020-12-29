a62ab2fa479242aee7dd6f7629ac3e77befb3d71
charlloss.dev <charlloss.dev@gmail.com>
2020-12-30 02:19:46 +0900

    refactor: position - line 양방관계 제거 리펙토링

--- src/main/java/jpa/position/Position.java
+++ src/main/java/jpa/position/Position.java
@@ -2,6 +2,7 @@ package jpa.position;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import jpa.common.BaseTime;
-import jpa.line.Line;
 import jpa.station.Station;
 import lombok.Getter;
 import lombok.ToString;
@@ -24,31 +24,29 @@ public class Position extends BaseTime {
 	private Long id;
 
 	@Getter
-	@ManyToOne
-	@JoinColumn
-	private Line line;
-
-	@Getter
-	@ManyToOne
-	@JoinColumn
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(nullable = false)
 	private Station station;
 
 	@Getter
 	@Column(nullable = false)
-	private int location;
+	private long lineId;
+
+	@Getter
+	@Column(nullable = false)
+	private long distance;
 
 	protected Position() {
 	}
 
-	public Position(int location) {
-		this.location = location;
+	public Position(Station station, long id, long distance) {
+		this.station = station;
+		this.lineId = id;
+		this.distance = distance;
 	}
 
-	public void addSubway(Station station, Line line) {
+	public void addStation(final Station station) {
 		station.getPositions().add(this);
-		line.getPositions().add(this);
-
 		this.station = station;
-		this.line = line;
 	}
 }
