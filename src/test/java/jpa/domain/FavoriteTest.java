9e53bcc445b277648118aa32251dbbddf725c883
Mingyu Lee <leemingyu05@gmail.com>
2020-12-30 19:07:25 +0900

    test: 사용자 조회시 가지고 있는 즐겨찾기 확인
    * favorite 생성자 변경으로 인한 영향 수정

--- src/test/java/jpa/domain/FavoriteTest.java
+++ src/test/java/jpa/domain/FavoriteTest.java
@@ -3,23 +3,49 @@ package jpa.domain;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import jpa.repository.FavoriteRepository;
+import jpa.repository.MemberRepository;
+import jpa.repository.StationRepository;
 
 @DataJpaTest
 public class FavoriteTest {
 
 	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private StationRepository stationRepository;
+
+	@Autowired
 	private FavoriteRepository favoriteRepository;
+
+	private Member member;
+
+	private Station sadang;
+
+	private Station gangnam;
+
+	@BeforeEach
+	public void setup() {
+		Member member = new Member("test@test.com", "1234", 32);
+		this.member = memberRepository.saveAndFlush(member);
+
+		Station sadang = new Station("사당역");
+		Station gangnam = new Station("강남역");
+		this.sadang = stationRepository.saveAndFlush(sadang);
+		this.gangnam = stationRepository.saveAndFlush(gangnam);
+	}
 
 	@DisplayName("Favorite 생성")
 	@Test
 	void given_favorite_when_save_then_return_created_favorite() {
-		Favorite favorite = new Favorite();
+		Favorite favorite = new Favorite(member, sadang, gangnam);
 
 		Favorite actual = favoriteRepository.save(favorite);
 
@@ -29,7 +55,7 @@ public class FavoriteTest {
 	@DisplayName("Favorite 조회")
 	@Test
 	void given_favorite_when_save_and_findById_then_return_created_favorite() {
-		Favorite favorite = new Favorite();
+		Favorite favorite = new Favorite(member, sadang, gangnam);
 		Favorite createdFavorite = favoriteRepository.save(favorite);
 
 		Favorite actual = favoriteRepository.findById(1L)
@@ -39,6 +65,19 @@ public class FavoriteTest {
 			() -> assertThat(actual.getId()).isEqualTo(createdFavorite.getId()),
 			() -> assertThat(actual == favorite).isTrue()
 		);
+	}
+
+	@DisplayName("사용자를 조회시 Favorite 조회")
+	@Test
+	void test() {
+		Favorite favorite = new Favorite(member, sadang, gangnam);
+		Favorite createdFavorite = favoriteRepository.save(favorite);
+
+		Member member = memberRepository.findByEmail("test@test.com")
+			.orElseThrow(IllegalArgumentException::new);
+
+		assertThat(member.getFavorites()).contains(createdFavorite);
+
 	}
 
 }
