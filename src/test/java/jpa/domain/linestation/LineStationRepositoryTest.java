21bba8d856e2453865a02fc4a2a4c7870ba507e0
Do Yeon Kim <chikeem90@gmail.com>
2020-12-31 15:56:58 +0900

    test: 중간에 station 추가되는 테스트 케이스 추가

--- src/test/java/jpa/domain/linestation/LineStationRepositoryTest.java
+++ src/test/java/jpa/domain/linestation/LineStationRepositoryTest.java
@@ -32,6 +32,7 @@ class LineStationRepositoryTest {
 		String expectedStationName = "강남역";
 		String expectedLineColor = "green";
 		String expectedLineName = "2호선";
+		int expectedDistance = 20;
 		Line line = new Line(expectedLineColor, expectedLineName);
 		Station previousStation = new Station(expectedPreviousStationName);
 		Station station = new Station(expectedStationName);
@@ -57,6 +58,39 @@ class LineStationRepositoryTest {
 		assertThat(expectedLineName).isEqualTo(findPreviousLineStation.getLine().getName());
 		assertThat(expectedLineColor).isEqualTo(findLineStation.getLine().getColor());
 		assertThat(expectedLineName).isEqualTo(findLineStation.getLine().getName());
+		assertThat(expectedDistance).isEqualTo(findLineStation.getDistance());
 	}
 
+	@Test
+	public void updateLineStation() {
+		// given
+		String expectedStationName = "교대역";
+		int expectedDistance = 30;
+		Line line = new Line("green", "2호선");
+		Station previousStation = new Station("서초역");
+		Station station = new Station("강남역");
+
+		lines.save(line);
+		stations.save(station);
+		stations.save(previousStation);
+		lineStations.save(new LineStation(line, previousStation, null, 0));
+		Long lineStationId = lineStations.save(new LineStation(line, station, previousStation, 50)).getId();
+
+		// when
+		Station newStation = new Station("교대역");
+		stations.save(newStation);
+		LineStation newLineStation = new LineStation(line, newStation, previousStation, 20);
+		lineStations.save(newLineStation);
+
+		LineStation lineStation = lineStations.findById(lineStationId)
+			.orElseThrow(() -> new IllegalArgumentException("아이디에 해당하는 데이터가 없습니다."));
+		lineStation.setPreviousStation(newStation);
+		lineStation.setDistance(30);
+
+		// then
+		LineStation findLineStation = lineStations.findById(lineStationId)
+			.orElseThrow(() -> new IllegalArgumentException("아이디에 해당하는 데이터가 없습니다."));
+		assertThat(expectedStationName).isEqualTo(findLineStation.getPreviousStation().getName());
+		assertThat(expectedDistance).isEqualTo(findLineStation.getDistance());
+	}
 }
