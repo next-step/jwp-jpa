68b76c96b4084f9d0ac8789272480814614bf814
charlloss.dev <charlloss.dev@gmail.com>
2021-01-04 17:47:56 +0900

    fix: 위치 생성 테스트 수정

--- src/test/java/jpa/position/PositionTest.java
+++ src/test/java/jpa/position/PositionTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,29 +27,34 @@ class PositionTest {
 	@Autowired
 	private PositionRepository positionRepository;
 
-	private Station 시청;
-
-	private Line lineNumber2;
-
-	@BeforeEach
-	void init() {
-		시청 = stationRepository.save(new Station("시청"));
-		lineNumber2 = lineRepository.save(new Line("2호선", Color.GREEN));
-		stationRepository.flush();
-	}
-
 	@Test
 	@DisplayName("포지션 생성 테스트")
 	void initPositionTest() {
+		// given
+		Station 강남 = 전철_역_생성("강남");
+		Station 서초 = 전철_역_생성("서초");
+		Line line2 = 전철_노선_생성("2호선", Color.GREEN);
 
-		System.out.println(lineNumber2.getId());
-		Position actual = positionRepository.save(new Position(시청, lineNumber2.getId(), 10L));
+		// when
+		Position expected = new Position(line2, 강남, 서초, 10L);
+		Position actual = positionRepository.save(expected);
 
+		// then
 		assertAll(
-			() -> assertThat(actual.getId()).isNotNull(),
-			() -> assertThat(actual.getDistance()).isEqualTo(10L),
-			() -> assertThat(actual.getStation()).isEqualTo(시청)
+			() -> assertThat(actual).isNotNull(),
+			() -> assertThat(actual.getLine()).isNotNull(),
+			() -> assertThat(actual.getUpStation().getName()).isEqualTo("강남"),
+			() -> assertThat(actual.getDownStation().getName()).isEqualTo("서초"),
+			() -> assertThat(actual.getDistance()).isEqualTo(10L)
 		);
+	}
+
+	private Station 전철_역_생성(String name) {
+		return stationRepository.save(new Station(name));
+	}
+
+	private Line 전철_노선_생성(String name, Color color) {
+		return lineRepository.save(new Line(name, color));
 	}
 }
 
