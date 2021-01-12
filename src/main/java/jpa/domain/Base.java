d17bd5be92a7b14e5cac85d839c5ed3db836349b
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-01-12 15:46:18 +0900

    refactor - Base 클래스 추상클래스로 변경

--- src/main/java/jpa/domain/Base.java
+++ src/main/java/jpa/domain/Base.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class Base {
+public abstract class Base {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
